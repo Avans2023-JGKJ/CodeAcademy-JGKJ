ea74109029cb15da9db1be6fe4d037c89fcbfbd7
Jochem-tb <jochem.welp@gmail.com>
2023-11-17 21:00:12 +0100

    Created Module.java & Webcast.java
    Java bestanden gemaakt m.b.v. Class Diagram

---
+++ Java/CodeAcademy-JGKJ/src/main/java/com/mycompany/codeacademy/jgkj/Module.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.codeacademy.jgkj;
+
+/**
+ *
+ * @author Jochem
+ */
+public class Module {
+
+    private String versie;
+    private String Beschrijving;
+    private String naamContactPersoon;
+    private String emailContactPersoon;
+    private byte volgordeNr;
+
+    public Module(String versie, String Beschrijving, String naamContactPersoon) {
+        this.versie = versie;
+        this.Beschrijving = Beschrijving;
+        this.naamContactPersoon = naamContactPersoon;
+        this.emailContactPersoon = "CONNECTIE MET DATABASE, COMBINATIE VAN NAAM!";
+    }
+
+    public Module(String versie, String Beschrijving, String naamContactPersoon, String emailContactPersoon) {
+        if (contactPersoonControle(naamContactPersoon)) {
+            this.versie = versie;
+            this.Beschrijving = Beschrijving;
+            this.naamContactPersoon = naamContactPersoon;
+            this.emailContactPersoon = emailContactPersoon;
+        }
+        else System.out.println("ERROR contactPersoonControle METHODE");
+    }
+
+    @Override
+    public String toString() {
+        return "Module{" + "versie=" + versie + ", Beschrijving=" + Beschrijving + ", naamContactPersoon=" + naamContactPersoon + ", emailContactPersoon=" + emailContactPersoon + ", volgordeNr=" + volgordeNr + '}';
+    }
+
+    private boolean contactPersoonControle(String naamContact) {
+        return true;
+        // HIER KOMT PROGRAMMA
+        // Naam van contactpersoon wordt gecontroleerd in database, indien aanwezig, geeft true terug. 
+        // Indien naam niet aanwezig, return false.
+    }
+
+    public String getVersie() {
+        return versie;
+    }
+
+    public String getBeschrijving() {
+        return Beschrijving;
+    }
+
+    public String getNaamContactPersoon() {
+        return naamContactPersoon;
+    }
+
+    public String getEmailContactPersoon() {
+        return emailContactPersoon;
+    }
+
+    public byte getVolgordeNr() {
+        return volgordeNr;
+    }
+
+    public void setVersie(String versie) {
+        this.versie = versie;
+    }
+
+    public void setBeschrijving(String Beschrijving) {
+        this.Beschrijving = Beschrijving;
+    }
+
+    public void setNaamContactPersoon(String naamContactPersoon) {
+        this.naamContactPersoon = naamContactPersoon;
+    }
+
+    public void setEmailContactPersoon(String emailContactPersoon) {
+        this.emailContactPersoon = emailContactPersoon;
+    }
+
+    public void setVolgordeNr(byte volgordeNr) {
+        this.volgordeNr = volgordeNr;
+    }
+
+}
---
+++ Java/CodeAcademy-JGKJ/src/main/java/com/mycompany/codeacademy/jgkj/Webcast.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.codeacademy.jgkj;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Jochem
+ */
+public class Webcast {
+
+    private short Tijdsduur;
+    private LocalDate DatumPublicatie;
+    private String URL;
+    private String Beschrijving;
+    private String NaamSpreker;
+    private String OrganisatieSpreker;
+    
+// CONSTRUCTORS -----------------------------------------------------------------
+
+    public Webcast(short Tijdsduur, LocalDate DatumPublicatie, String URL, String Beschrijving, String NaamSpreker) {
+        this.Tijdsduur = Tijdsduur;
+        this.DatumPublicatie = DatumPublicatie;
+        this.URL = URL;
+        this.Beschrijving = Beschrijving;
+        this.NaamSpreker = NaamSpreker;
+    }
+
+    public Webcast(short Tijdsduur, LocalDate DatumPublicatie, String URL, String Beschrijving, String NaamSpreker, String OrganisatieSpreker) {
+        this.Tijdsduur = Tijdsduur;
+        this.DatumPublicatie = DatumPublicatie;
+        this.URL = URL;
+        this.Beschrijving = Beschrijving;
+        this.NaamSpreker = NaamSpreker;
+        this.OrganisatieSpreker = "CONNECTIE MET DATABASE, COMBINATIE VAN NAAM!";
+    }
+
+// GETTERS -----------------------------------------------------------------
+    public short getTijdsduur() {
+        return Tijdsduur;
+    }
+
+    public LocalDate getDatumPublicatie() {
+        return DatumPublicatie;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public String getBeschrijving() {
+        return Beschrijving;
+    }
+
+    public String getNaamSpreker() {
+        return NaamSpreker;
+    }
+
+    public String getOrganisatieSpreker() {
+        return OrganisatieSpreker;
+    }
+
+// SETTERS -----------------------------------------------------------------
+    public void setTijdsduur(short Tijdsduur) {
+        this.Tijdsduur = Tijdsduur;
+    }
+
+    public void setDatumPublicatie(LocalDate DatumPublicatie) {
+        this.DatumPublicatie = DatumPublicatie;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public void setBeschrijving(String Beschrijving) {
+        this.Beschrijving = Beschrijving;
+    }
+
+    public void setNaamSpreker(String NaamSpreker) {
+        this.NaamSpreker = NaamSpreker;
+    }
+
+    public void setOrganisatieSpreker(String OrganisatieSpreker) {
+        this.OrganisatieSpreker = OrganisatieSpreker;
+    }
+
+}
