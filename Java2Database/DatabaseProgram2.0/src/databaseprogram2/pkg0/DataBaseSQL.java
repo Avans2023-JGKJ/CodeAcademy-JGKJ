340a82f7e7b840117d7c163155694a07b9fd21a3
Jochem-tb <jochem.welp@gmail.com>
2023-11-24 18:00:50 +0100

    Create genInsert, Update Connection,  Create WireFrame
    Veel gedaan:
    Generator Insert, vermakkelijkt.
    Update Connection, naar nieuwe database.
    WireFrame gemaakt voor JavaFX, oftewel GUI van applicatie. Zie mapje JavaFX
    Bovenstaande gemaakt in samenwerking met Gijs

--- Java2Database/DatabaseProgram2.0/src/databaseprogram2/pkg0/DataBaseSQL.java
+++ Java2Database/DatabaseProgram2.0/src/databaseprogram2/pkg0/DataBaseSQL.java
@@ -1,11 +1,13 @@
 package databaseprogram2.pkg0;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseSQL {
-private static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+
+    private static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     private static final String DB_URL = "jdbc:sqlserver://aei-sql2.avans.nl:1443;DatabaseName=CodeAcademyJGKJ;encrypt=false;";
     private static final String USER = "jgkj";
     private static final String PASSWORD = "jgkj2023";
@@ -20,14 +22,18 @@ private static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServe
             System.out.println("Connection made successfully!");
             return connection;
         } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("An Error has occured, tyring to connect to the database:");
+            System.out.println("An ERROR has occured, tyring to connect to the database:");
             e.printStackTrace(); // Als er een exeptie optreed, mooi afhandelen.
             return null;
         }
     }
+
+    public static void sendCommand(Connection connection, String command) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(command);
+            System.out.println("Command has been executed.");
+        }
+    }
+    
+  
 }
-
-   
-    
-    
-
