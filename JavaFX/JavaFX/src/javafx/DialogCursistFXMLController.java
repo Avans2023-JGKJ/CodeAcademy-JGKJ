ed29650b20a347dcb9c0d65d61631a1f65d49923
jhm162 <j.hadjmessaoud@student.avans.nl>
2023-12-07 15:32:06 +0100

    Cursist 100% Afgemaakt
    1. Je kan nu cursisten aanmaken
    2. Je kan cursisten aanpassen
    3. Je kan cursisten verwijderen
    
    Geslacht en geboortedatum werken eindelijk.
    -> Geslacht is geselecteerd als je een cursist bekijkt
    -> Je kan maar 1 geslacht selecteren
    -> Geslacht kan je nu ook aanpassen

--- JavaFX/JavaFX/src/javafx/DialogCursistFXMLController.java
+++ JavaFX/JavaFX/src/javafx/DialogCursistFXMLController.java
@@ -12,6 +12,8 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
 public class DialogCursistFXMLController implements Initializable {
 
@@ -20,6 +22,9 @@ public class DialogCursistFXMLController implements Initializable {
 
     @FXML
     TextField cursistPostcodeInput;
+
+    @FXML
+    TextField cursistGeboortedatumInput;
 
     @FXML
     TextField cursistEmailInput;
@@ -33,48 +38,76 @@ public class DialogCursistFXMLController implements Initializable {
     @FXML
     TextField cursistLandCodeInput;
 
+    @FXML
+    RadioButton radioButtonCursistMan, radioButtonCursistVrouw;
+    
+    ToggleGroup group = new ToggleGroup();
+    
+
+
 //       @FXML
     void FinishButtonCreateCursistClicked() {
-        try {
-            DataBaseSQL.sendCommand(DataBaseSQL.createConnection(),
-                    "INSERT INTO Cursist (naam,postCode,email,huisNummer,woonPlaats,landCode) VALUES"
+            try {
+
+                DataBaseSQL.sendCommand(DataBaseSQL.createConnection(),
+                        "INSERT INTO Cursist (naam,postCode,email,geboorteDatum,geslacht,huisNummer,woonPlaats,landCode) VALUES("
                     + "  '" + cursistNaamColumninput.getText()
                     + "',  '" + cursistPostcodeInput.getText()
                     + "',  '" + cursistEmailInput.getText()
+                    + "',  '" + cursistGeboortedatumInput.getText()
+                    + "',  '" + RadioButtonGeslachtCheck()
                     + "',   '" + cursistHuisnummerInput.getText()
                     + "',  '" + cursistWoonplaatsInput.getText()
-                    + "',  '" + cursistLandCodeInput.getText());
+                    + "',  '" + cursistLandCodeInput.getText() + "')");
 
-            System.out.println("SUCCESVOL GEDAAN");
-
-        } catch (SQLException ex) {
-            Logger.getLogger(DialogCursistFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(DialogCursistFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
+    
+    public char RadioButtonGeslachtCheck() {
+        if (radioButtonCursistMan.isSelected()) {
+            return 'm';
+        }
+        else if (radioButtonCursistVrouw.isSelected()) {
+            return 'v';
+        }
+        return 0;
     }
 
-    @FXML
-    void ApplyButtonUpdateCursistClicked() {
+        @FXML
+        void ApplyButtonUpdateCursistClicked
+        
+            () {
         try {
-            DataBaseSQL.sendCommand(DataBaseSQL.createConnection(),
-                    "UPDATE Cursist SET"
-                    + " naam = '" + cursistNaamColumninput.getText()
-                    + "', postCode = '" + cursistPostcodeInput.getText()
-                    + "', email = '" + cursistEmailInput.getText()
-                    + "', huisNummer = '" + cursistHuisnummerInput.getText()
-                    + "', woonPlaats = '" + cursistWoonplaatsInput.getText()
-                    + "', landCode = '" + cursistLandCodeInput.getText()
-                    + "' WHERE email = '" + DataShare.getInstance().getCursistEmail() + "'");
-            System.out.println("SUCCESVOL GEDAAN");
+                DataBaseSQL.sendCommand(DataBaseSQL.createConnection(),
+                        "UPDATE Cursist SET"
+                        + " naam = '" + cursistNaamColumninput.getText()
+                        + "', postCode = '" + cursistPostcodeInput.getText()
+                        + "', email = '" + cursistEmailInput.getText()
+                        + "', geslacht = '" + RadioButtonGeslachtCheck()
+                        + "', huisNummer = '" + cursistHuisnummerInput.getText()
+                        + "', woonPlaats = '" + cursistWoonplaatsInput.getText()
+                        + "', landCode = '" + cursistLandCodeInput.getText()
+                        + "' WHERE email = '" + DataShare.getInstance().getCursistEmail() + "'");
+               
 
-        } catch (SQLException ex) {
-            Logger.getLogger(DialogCursistFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(DialogCursistFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-    }
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+        @Override
+        public void initialize
+        (URL url, ResourceBundle rb
+        
+            ) {
         loadData();
-    }
+        radioButtonCursistMan.setToggleGroup(group);
+        radioButtonCursistVrouw.setToggleGroup(group);
+        }
+
+    
 
     private void loadData() {
         cursistNaamColumninput.setText(String.valueOf(DataShare.getInstance().getCursistNaam()));
@@ -83,5 +116,15 @@ public class DialogCursistFXMLController implements Initializable {
         cursistHuisnummerInput.setText(String.valueOf(DataShare.getInstance().getCursistHuisnummer()));
         cursistWoonplaatsInput.setText(String.valueOf(DataShare.getInstance().getCursistWoonPlaats()));
         cursistLandCodeInput.setText(String.valueOf(DataShare.getInstance().getCursistLandCode()));
+        if (DataShare.getInstance().getCursistGeslacht() == 'm'){
+        radioButtonCursistMan.setSelected(true);
+        radioButtonCursistVrouw.setSelected(false);
+    }
+        else if (DataShare.getInstance().getCursistGeslacht() == 'v'){
+        radioButtonCursistMan.setSelected(false);
+        radioButtonCursistVrouw.setSelected(true);
+    }
+        
+        
     }
 }
